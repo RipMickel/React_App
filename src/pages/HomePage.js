d2f036ad87506664cf99396fdf92c1c540f7e794
RipMickel <gaudicosmickelangelo@gmail.com>
2025-04-28 10:02:16 +0800

    Create HomePage.js with welcome message for chat app.

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to the Chat App!</h1>
+      <p>Start chatting with your friends 🚀</p>
+    </div>
+  );
+};
+
+export default HomePage;
