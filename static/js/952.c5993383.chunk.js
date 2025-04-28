ad07551e38af14219e386aa8fa81288a781b2565
RipMickel <gaudicosmickelangelo@gmail.com>
2025-04-28 10:12:50 +0800

    Updates

---
+++ static/js/952.c5993383.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkchat_app=self.webpackChunkchat_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.c5993383.chunk.js.map
